8bd3bd08d392dd373f7ff496ef2e3f81646f209b
zach <zach@DESKTOP-81OE9FR>
2017-11-08 16:28:49 -0600

    Updated Detail Action method to use action result type

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,11 +6,27 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
+    //action controller using System.Web.Mvc
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        //public method to give detail
+        //ActionReuslt type is usually action return type press alt f12 if there is an error
+        public ActionResult Detail()
         {
-            return "hello from the comic book controller!";
+            
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                //redirect is a method in Controller class of mvc
+                return Redirect("/");
+                
+            }
+
+            //content is method isController class of mvc
+            return Content("Hello from the comic book controller!");
+           
+
+        
+        
         }
     }
 }
